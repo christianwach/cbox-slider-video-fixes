f4101beae42ac2f3a8b9a2fbf2bbf40951c5f84b
Christian Wach <needle@haystack.co.uk>
2015-11-18 11:18:07 +0000

    spruce up the comments

--- cbox-slider-video-fixes.php
+++ cbox-slider-video-fixes.php
@@ -1,16 +1,18 @@
-<?php
-/*
+<?php /*
+--------------------------------------------------------------------------------
 Plugin Name: CBOX Slider Video Fixes
-Plugin URI: http://haystack.co.uk
+Plugin URI: https://github.com/christianwach/cbox-slider-video-fixes
 Description: Fixes the handling of YouTube video in the default slider provided by the CBOX Theme
 Author: Christian Wach
-Version: 0.1
+Version: 0.2
 Author URI: http://haystack.co.uk
+--------------------------------------------------------------------------------
 */
 
 
+
 /**
- * Enqueue Javascript
+ * Enqueue Javascript.
  */
 function cbox_slider_video_fixes_enqueue_scripts() {
 
@@ -36,7 +38,13 @@ add_action( 'wp_enqueue_scripts', 'cbox_slider_video_fixes_enqueue_scripts' );
 
 
 /**
- * Filter YouTube URL to enable API
+ * Filter YouTube URL to enable API.
+ *
+ * @param str $html The existing oEmbed HTML
+ * @param str $url The YouTube video URL
+ * @param str $attr The oEmbed attributes
+ * @param int $post_ID The containing WordPress post ID
+ * @return str $html The modified oEmbed HTML
  */
 function cbox_slider_video_fixes_wp_embed_handler_youtube( $html, $url, $attr, $post_ID ) {
 
@@ -46,7 +54,7 @@ function cbox_slider_video_fixes_wp_embed_handler_youtube( $html, $url, $attr, $
 	// is this a featured slider?
 	if ( $post_type == 'features' ) {
 
-		// do we have an oembed?
+		// do we have an oEmbed?
 		if ( false !== strpos( $html, 'feature=oembed' ) ) {
 
 			// enable YouTube API on video
@@ -68,7 +76,13 @@ add_filter( 'embed_oembed_html', 'cbox_slider_video_fixes_wp_embed_handler_youtu
 
 
 /**
- * Customise the CBOX Slider
+ * Customise the CBOX Slider.
+ *
+ * We need to show the slider controls because users need to be able to manually
+ * navigate the slider when videos are present.
+ *
+ * @param array $args The existing setup arguments that are passed to the CBOX slider
+ * @return array $args The modified setup arguments to be passed to the CBOX slider
  */
 function cbox_slider_video_fixes_flex_slider_controls( $args ) {
 
